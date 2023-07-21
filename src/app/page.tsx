8b437ca02f3eb6048a5a48639eb34bbad62b43ea
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2023-07-21 21:00:00 +0900

    update layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,9 @@
-import { FiSearch, FiGithub, FiTwitter } from "react-icons/fi";
-import { Header } from "@/components/Header";
 import { SearchBar } from "@/components/SearchBar";
 
 export default function Home() {
   return (
-    <div className="h-screen bg-gray-200 flex flex-col">
-      <Header />
-      <main className="flex-grow flex items-center justify-center">
-        <SearchBar />
-      </main>
-    </div>
+    <main className="flex-grow flex items-center justify-center">
+      <SearchBar />
+    </main>
   );
 }
