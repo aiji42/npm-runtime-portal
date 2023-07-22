47e7fa8e4b71794ed44f90038e190569dd815e44
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2023-07-22 20:59:56 +0900

    add clerk

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
-import { FiGithub, FiTwitter } from "react-icons/fi";
 import { SearchBar } from "@/components/SearchBar";
 import { ComponentProps } from "react";
 import Link from "next/link";
+import { UserButton } from "@clerk/nextjs";
 
 type Props = {
   withSearch?: boolean;
@@ -10,36 +10,19 @@ type Props = {
 export const Header = ({ withSearch, defaultValue }: Props) => {
   return (
     <header className="bg-sky-950 text-white p-3 flex justify-between items-center flex-row-reverse h-14">
-      <div className="flex">
-        <a
-          href="https://github.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="mr-4 hover:text-gray-300"
-          title="repository"
-        >
-          <FiGithub size={24} />
-        </a>
-        <a
-          href="https://twitter.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="hover:text-gray-300"
-          title="twitter"
-        >
-          <FiTwitter size={24} />
-        </a>
+      <div className="flex gap-4 items-center">
+        <UserButton afterSignOutUrl="/" />
       </div>
-      {withSearch && (
-        <div className="flex gap-3 items-center">
-          <Link href="/">
-            <h1 className="text-3xl font-bold text-white">Runtimes</h1>
-          </Link>
+      <div className="flex gap-3 items-center">
+        <Link href="/">
+          <h1 className="text-3xl font-bold text-white">Runtimes</h1>
+        </Link>
+        {withSearch && (
           <div className="max-w-md">
             <SearchBar narrow defaultValue={defaultValue} />
           </div>
-        </div>
-      )}
+        )}
+      </div>
     </header>
   );
 };
